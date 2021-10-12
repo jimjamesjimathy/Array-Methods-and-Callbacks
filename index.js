4029c6b50f80ddcfbeaaf8c99d674bfe9fd9ba57
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-10-12 14:57:30 -0400

    First commit. Task 2 complete. onwards and upwards.

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const { fifaData } = require('./fifa.js')
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
 //(a) Home Team name for 2014 world cup final
-
+const finals2014 = fifaData.filter(function(item){
+    return item['Year'] === 2014 && item['Stage'] === 'Final';
+ });
+ console.log('Task 1a', finals2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log('Task 1b', finals2014[0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
-
+console.log('Task 1c', finals2014[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
-
+console.log('Task 1d', finals2014[0]['Away Team Goals']);
 //(e) Winner of 2014 world cup final */
-
+console.log('Task 1e', finals2014[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -23,9 +26,12 @@ Use getFinals to do the following:
 
 hint - you should be looking at the stage key inside of the objects
 */
-
-function getFinals(/* code here */) {
-   /* code here */
+// .filter()
+function getFinals(fifaData) {
+    const finalTeams = fifaData.filter(function(item){
+        return item['Stage'] === 'Final';
+    })
+    return finalTeams;
 }
 
 
@@ -35,6 +41,8 @@ Use the higher-order function called getYears to do the following:
 1. Receive an array
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
+
+
 
 function getYears(/* code here */) {
     /* code here */
@@ -64,7 +72,7 @@ Use the higher-order function getWinnersByYear to do the following:
 
 hint: the strings returned need to exactly match the string in step 4.
  */
-
+// .map()
 function getWinnersByYear(/* code here */) {
     /* code here */
 }
